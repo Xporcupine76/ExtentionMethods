43e0015f41444f8fe792e7bf4fac640a9fcfe5b9
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:03:34 +0200

    Add project files.

---
+++ ExtentionMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace ExtentionMethods
+{
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            string x = "Hello world";
+            string y = "hello";
+            Console.WriteLine(x + " is captialized " + x.IsCap());
+            Console.WriteLine(y + " is captialized " + y.IsCap());
+
+        }
+
+    }
+    public static class StringCap
+    {
+        public static bool IsCap(this string s)
+        {
+
+            if(string.IsNullOrEmpty(s)) return false;
+            return char.IsUpper(s[0]);    
+
+
+        }
+
+    }
+}
+
